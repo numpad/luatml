9b72dabb4168ece2d13510a326673d353f8345b3
Christian Schäl <christian.schael87@gmail.com>
2023-11-10 00:05:39 +0100

    fixed listfiles for non-existing dir

--- src/luatml_lib.c
+++ src/luatml_lib.c
@@ -16,7 +16,12 @@ static int luatml_lib_listfiles(lua_State *L) {
 	lua_newtable(L);
 
 	luatmlfs_iterator it;
-	luatmlfs_iterator_init(&it, arg_path);
+	const LUATML_RESULT_TYPE result = luatmlfs_iterator_init(&it, arg_path);
+	if (result != LUATML_RESULT_OK) {
+		luaL_error(L, "Parameter is not a valid directory: '%s'.", arg_path);
+		return 0;
+	}
+
 	it.mask = LUATMLFS_FILE;
 
 	int table_index = 1;
